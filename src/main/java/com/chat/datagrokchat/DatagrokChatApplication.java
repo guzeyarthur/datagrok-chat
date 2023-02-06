ddfe2b44828f02f18842f649ca9cfe2c568b3fb1
Artur Guzey <artur.guzey@gmail.com>
2023-02-07 00:25:41 +0200

    Init project

---
+++ src/main/java/com/chat/datagrokchat/DatagrokChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.datagrokchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatagrokChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatagrokChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chat/datagrokchat/DatagrokChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.datagrokchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatagrokChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
